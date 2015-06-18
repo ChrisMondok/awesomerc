ddfe848d41068a62b70d2cbc173749d3a191c696
Chris Mondok <chris.mondok@gmail.com>
2015-06-17 22:46:13 -0400

    Use shift, not ctrl, for restart awesome

--- rc.lua
+++ rc.lua
@@ -278,7 +278,7 @@ globalkeys = awful.util.table.join(
 
 	-- Standard program
 	awful.key({ modkey,           }, "Return",  function () awful.util.spawn(terminal) end),
-	awful.key({ modkey, "Control" }, "r",       awesome.restart),
+	awful.key({ modkey, "Shift"   }, "r",       awesome.restart),
 	awful.key({ modkey, "Shift"   }, "q",       awesome.quit),
 
 	awful.key({ modkey,           }, "l",       function () awful.tag.incmwfact( 0.05) end),
